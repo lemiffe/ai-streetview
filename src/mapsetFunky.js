21022e087d7205c5803c5d85bb1611e1d2fb32a6
Mike Warner <michael.warner@showpad.com>
2022-10-19 16:45:14 +0200

    Funky dataset (2 first images)

---
+++ src/mapsetFunky.js
@@ -0,0 +1,24 @@
+const mapset = [
+    {
+        latlon: [-35.1825396, 149.2602411],
+        streetUrl:
+            "https://www.google.com/maps/@-35.1825396,149.2602411,3a,75y,349.78h,90.82t/data=!3m6!1e1!3m4!1sbglyqcCOfambSZfNBtBcxg!2e0!7i16384!8i8192",
+        images: ["_funky-1.jpg"],
+    },
+    {
+        latlon: [56.056227, 44.3989451],
+        streetUrl:
+            "https://www.google.com/maps/@56.056227,44.3989451,3a,75y,78.91h,93.66t/data=!3m6!1e1!3m4!1sQmRMjc96NgLWJucB5xNkwA!2e0!7i13312!8i6656",
+        images: ["_funky-2.jpg"],
+    },
+];
+
+export default mapset;
+
+/*
+    {
+        latlon: [],
+        streetUrl: "",
+        images: ["_funky-.jpg"],
+    },
+*/
