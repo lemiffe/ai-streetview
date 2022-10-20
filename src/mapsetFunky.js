054ee16b6a32870392ef0847faa4e3e809717e10
Mike Warner <michael.warner@showpad.com>
2022-10-20 21:32:27 +0200

    I really want these funky images to work

--- src/mapsetFunky.js
+++ src/mapsetFunky.js
@@ -3,31 +3,31 @@ const mapset = [
         latlon: [-35.1825396, 149.2602411],
         streetUrl:
             "https://www.google.com/maps/@-35.1825396,149.2602411,3a,75y,349.78h,90.82t/data=!3m6!1e1!3m4!1sbglyqcCOfambSZfNBtBcxg!2e0!7i16384!8i8192",
-        images: ["_funky-1.jpg"],
+        images: ["funky-1.jpg"],
     },
     {
         latlon: [56.056227, 44.3989451],
         streetUrl:
             "https://www.google.com/maps/@56.056227,44.3989451,3a,75y,78.91h,93.66t/data=!3m6!1e1!3m4!1sQmRMjc96NgLWJucB5xNkwA!2e0!7i13312!8i6656",
-        images: ["_funky-2.jpg"],
+        images: ["funky-2.jpg"],
     },
     {
         latlon: [36.9656236, -93.3821924],
         streetUrl:
             "https://www.google.com/maps/@36.9656236,-93.3821924,3a,75y,105.85h,90.7t/data=!3m6!1e1!3m4!1s1pK8kq_hEGL_EUU8rLIX0Q!2e0!7i13312!8i6656",
-        images: ["_funky-3.jpg"],
+        images: ["funky-3.jpg"],
     },
     {
         latlon: [23.8708433, 120.4801888],
         streetUrl:
             "https://www.google.com/maps/@23.8708433,120.4801888,3a,75y,274.62h,100.01t/data=!3m6!1e1!3m4!1sZPpLGPUnuR7LL3RoWo3vwg!2e0!7i16384!8i8192",
-        images: ["_funky-4.jpg"],
+        images: ["funky-4.jpg"],
     },
     {
         latlon: [17.3886774, 103.7217915],
         streetUrl:
             "https://www.google.com/maps/@17.3886774,103.7217915,3a,75y,17.45h,97.41t/data=!3m6!1e1!3m4!1sb8UUsrnk0hbVnLLIzshGnA!2e0!7i16384!8i8192",
-        images: ["_funky-5.jpg"],
+        images: ["funky-5.jpg"],
     },
 ];
 
