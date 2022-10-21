7e0fb210b8fa608a300754e1e3a57b19b1702710
Mike Warner <michael.warner@showpad.com>
2022-10-21 16:19:34 +0200

    5 more funky maps

--- src/mapsetFunky.js
+++ src/mapsetFunky.js
@@ -29,6 +29,36 @@ const mapset = [
             "https://www.google.com/maps/@17.3886774,103.7217915,3a,75y,17.45h,97.41t/data=!3m6!1e1!3m4!1sb8UUsrnk0hbVnLLIzshGnA!2e0!7i16384!8i8192",
         images: ["funky-5.jpg"],
     },
+    {
+        latlon: [22.5979195, 89.0018266],
+        streetUrl:
+            "https://www.google.com/maps/@22.5979195,89.0018266,3a,75y,160.79h,91.54t/data=!3m6!1e1!3m4!1snE3t9MXMDw2lerof677tLw!2e0!7i13312!8i6656",
+        images: ["funky-6.jpg"],
+    },
+    {
+        latlon: [39.13591, -7.5940821],
+        streetUrl:
+            "https://www.google.com/maps/@39.13591,-7.5940821,3a,75y,27.55h,92.83t/data=!3m6!1e1!3m4!1sbylxwRpZd1fboBS3EGqsdQ!2e0!7i13312!8i6656",
+        images: ["funky-7.jpg"],
+    },
+    {
+        latlon: [41.4740909, -97.6957617],
+        streetUrl:
+            "https://www.google.com/maps/@41.4740909,-97.6957617,3a,75y,248.15h,78.7t/data=!3m6!1e1!3m4!1swmUFoEpkbv3eZRuw3qy_JQ!2e0!7i13312!8i6656",
+        images: ["funky-8.jpg"],
+    },
+    {
+        latlon: [49.7135168, -107.2916169],
+        streetUrl:
+            "https://www.google.com/maps/@49.7135168,-107.2916169,3a,75y,11.86h,91.67t/data=!3m6!1e1!3m4!1s3wxMbgEtbU2Aq4s-a5jP4A!2e0!7i13312!8i6656",
+        images: ["funky-9.jpg"],
+    },
+    {
+        latlon: [-52.597774, -71.1984926],
+        streetUrl:
+            "https://www.google.com/maps/@-52.597774,-71.1984926,3a,75y,133.59h,93.19t/data=!3m6!1e1!3m4!1sSDwOPz6lXZk8cr_3ssK4CA!2e0!7i13312!8i6656",
+        images: ["funky-10.jpg"],
+    },
 ];
 
 export default mapset;
@@ -37,6 +67,6 @@ export default mapset;
 {
     latlon: [],
     streetUrl: "",
-    images: ["_funky-.jpg"],
+    images: ["funky-.jpg"],
 },
 */
